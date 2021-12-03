beae46fb3a3894b5dddf97768b03107acfe79869
khargr14 <kay_hargrove@yahoo.com>
2021-12-03 18:56:39 -0500

    add bad request error

--- dist/index.0e47d5c3.js
+++ dist/index.0e47d5c3.js
@@ -467,9 +467,10 @@ const timeout = function(s) {
 ///////////////////////////////////////
 const showRecipe = async function() {
     try {
-        const res = await fetch('https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886');
+        const res = await fetch('https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886gggggggggg');
         const data = await res.json();
         console.log(res, data);
+        if (!res.ok) throw new Error(`${data.message} (${res.status})`);
     } catch (err) {
         alert(err);
     }
--- src/js/controller.js
+++ src/js/controller.js
@@ -12,14 +12,16 @@ const timeout = function (s) {
 
 ///////////////////////////////////////
 
-const showRecipe = async function() {
+const showRecipe = async function () {
   try {
-    const res =  await fetch('https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886');
-      const data = await res.json();
+    const res = await fetch('https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886gggggggggg');
+    const data = await res.json();
 
-      console.log(res, data);
+    console.log(res, data);
 
-  }catch(err) {
+      if(!res.ok) throw new Error(`${data.message} (${res.status})`)
+
+  } catch (err) {
     alert(err);
   }
 };
