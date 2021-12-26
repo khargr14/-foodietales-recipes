f6817d623db35f4b3e1327b83e4d5f0297992993
khargr14 <kay_hargrove@yahoo.com>
2021-12-25 19:18:55 -0500

    add class RecipeView in recipeView

--- src/js/model.js
+++ src/js/model.js
@@ -10,8 +10,9 @@ export const loadRecipe = async function(id){
     
 
     const res = await fetch(
+     
     
-   `https://forkify-api.herokuapp.com/api/v2/recipes/${id}`
+         `https://forkify-api.herokuapp.com/api/v2/recipes/${id}`
     );
    const data = await res.json();
 
---
+++ src/js/views/recipeView.js
@@ -0,0 +1,6 @@
+
+class RecipeView {
+    #parentElement = document.querySelector('.recipe');
+}
+
+export default new RecipeView();
